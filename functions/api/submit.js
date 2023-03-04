541ffdfcd9fae4d8c7c52e376cf5158ac5062ed2
danialjose <danialjose@gmail.com>
2023-03-04 05:10:02 +0000

    added submit form js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,79 +1,89 @@
-/**
- * POST /api/submit
- */
- // Script configuration
- const config = {
-   api-key: "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",
-   content-type: "application/json",
-   accept: "application/json",
-   mailgun_domain: "https://api.sendinblue.com/v3/smtp/email ",
-   from: "no-reply <no-reply@danjose.pt>",
-   admin_email: "danialjose@gmail.com",
-   email_field: "email", // email field name
-   form_fields: ["name", "message"], // list of required fields
- };
 
+ export async function onRequestPost(context) {
+   try {
+     const { request } = context;
 
+     const { name, email, message } = await request.json();
 
+     const resp = await fetch(
+       new Request("https://api.sendinblue.com/v3/smtp/email", {
+         method: "POST",
+         headers: {
+           "content-type": "application/json",
+           "accept": "application/json",
+           "api-key": "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",
 
- curl --request POST \
-   --url https://api.sendinblue.com/v3/smtp/email \
-   --header 'accept: application/json' \
-   --header 'api-key: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' \
-   --header 'content-type: application/json' \
-   --data '{
-    "sender":{
-       "name":"Sender Alex",
-       "email":"senderalex@example.com"
-    },
-    "to":[
+         },
+         body: JSON.stringify({
+           personalizations: [
+             {
+               to: [
+                 {
+                   email: "opensource@thearcadia.xyz",
+                   name: "Name",
+                 },
+               ],
+             },
+           ],
+           from: {
+             email: "inquiry@opensource.express",
+             name,
+           },
+           subject,
+           content: [
+             {
+               type: "text/plain",
+               value: `From: ${email}: ${message}`,
+             },
+           ],
+         }),
+       })
+     );
+
+     const respContent = resp.status + " " + resp.statusText;
+
+     return new Response(
+       JSON.stringify({
+         success: true,
+         data: {
+           message: "Your message has been sent!",
+           respContent,
+         },
+       }),
        {
-          "email":"testmail@example.com",
-          "name":"John Doe"
+         headers: {
+           "content-type": "application/json",
+           "accept": "application/json",
+           "api-key": "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",
+
+           // Allow all cors
+           "Access-Control-Allow-Origin": "*",
+           // Allow all cors methods
+           "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+           // Allow all cors headers
+           "Access-Control-Allow-Headers": "*",
+         },
        }
-    ],
-    "subject":"test mail",
-    "htmlContent":"<html><head></head><body><h1>Hello this is a test email from sib</h1></body></html>",
-    "headers":{
-       "X-Mailin-custom":"custom_header_1:custom_value_1|custom_header_2:custom_value_2|custom_header_3:custom_value_3",
-       "charset":"iso-8859-1"
-    }
- }'
+     );
+   } catch (err) {
+     return new Response(
+       JSON.stringify({
+         success: false,
+         data: {
+           message: "Something went wrong!",
+           error: err.message,
+         },
+       }),
+       {
+         headers: {
+           "content-type": "application/json",
+           "accept": "application/json",
+           "api-key": "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",
 
-
-
-
- // --------
-export async function onRequestPost(context) {
-  try {
-    let input = await context.request.formData();
-
-    // Convert FormData to JSON
-    // NOTE: Allows multiple values per key
-    let output = {};
-    for (let [key, value] of input) {
-      let tmp = output[key];
-      if (tmp === undefined) {
-        output[key] = value;
-      } else {
-        output[key] = [].concat(tmp, value);
-      }
-    }
-
-    let pretty = JSON.stringify(output, null, 2);
-    return new Response(pretty, {
-      headers: {
-        'api-key': "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",
-        'Content-Type': 'application/json',
-        'accept': "application/json",
-        'mailgun_domain': "https://api.sendinblue.com/v3/smtp/email ",
-        'from': "no-reply <no-reply@danjose.pt>",
-        'admin_email': "danialjose@gmail.com",
-        'email_field': "email", // email field name
-        'form_fields': ["name", "message"], // list of required fields
-      },
-    });
-  } catch (err) {
-    return new Response('Error parsing JSON content', { status: 400 });
-  }
-}
+           // Allow all cors
+           "Access-Control-Allow-Origin": "*",
+           // Allow all cors methods
+           "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+           // Allow all cors headers
+           "Access-Control-Allow-Headers": "*",
+         },

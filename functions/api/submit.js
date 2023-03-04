16f8f53545a82a00beeb9eab37d9a6d9f3541046
danialjose <danialjose@gmail.com>
2023-03-04 04:40:02 +0000

    added submit form js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -13,6 +13,36 @@
    form_fields: ["name", "message"], // list of required fields
  };
 
+
+
+
+ curl --request POST \
+   --url https://api.sendinblue.com/v3/smtp/email \
+   --header 'accept: application/json' \
+   --header 'api-key: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' \
+   --header 'content-type: application/json' \
+   --data '{
+    "sender":{
+       "name":"Sender Alex",
+       "email":"senderalex@example.com"
+    },
+    "to":[
+       {
+          "email":"testmail@example.com",
+          "name":"John Doe"
+       }
+    ],
+    "subject":"test mail",
+    "htmlContent":"<html><head></head><body><h1>Hello this is a test email from sib</h1></body></html>",
+    "headers":{
+       "X-Mailin-custom":"custom_header_1:custom_value_1|custom_header_2:custom_value_2|custom_header_3:custom_value_3",
+       "charset":"iso-8859-1"
+    }
+ }'
+
+
+
+
  // --------
 export async function onRequestPost(context) {
   try {

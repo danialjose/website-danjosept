bd1129cca665aca857dc7086053fbff3f6e4dd68
danialjose <danialjose@gmail.com>
2023-03-04 04:33:31 +0000

    added submit form js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,6 +1,19 @@
 /**
  * POST /api/submit
  */
+ // Script configuration
+ const config = {
+   api-key: "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",
+   content-type: "application/json",
+   accept: "application/json",
+   mailgun_domain: "https://api.sendinblue.com/v3/smtp/email ",
+   from: "no-reply <no-reply@danjose.pt>",
+   admin_email: "danialjose@gmail.com",
+   email_field: "email", // email field name
+   form_fields: ["name", "message"], // list of required fields
+ };
+
+ // --------
 export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
